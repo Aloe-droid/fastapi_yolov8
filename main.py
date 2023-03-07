e6a58be9d91e96913a676b864f5bcfef80a78ba2
Yurve <https://github.com/Yurve>
2023-03-07 17:11:29 +0900

    test

--- main.py
+++ main.py
@@ -1,57 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.exceptions import HTTPException
-from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from serializers import Event
 from ultralytics import YOLO
 from PIL import Image
 
 app = FastAPI() 
-
-# @app.post("/api/event/events")
-# async def create_event(event: Event):
-#     if event.EventHeader is None:
-#         raise HTTPException(status_code=422, detail="EventHeader is required")
-    
-#     model = YOLO("yolov8l.pt")
-
-#     try:
-#         userId = event.EventHeader.UserId
-#         cameraId = event.EventHeader.CameraId
-#         created = event.EventHeader.Created
-#         path = event.EventHeader.Path
-#         image = Image.open(path)
-
-#         results = model.predict(image)
-
-#         event_bodies = []
-#         for result in results:
-#             for bbox, cls in zip(result.boxes.xyxy, result.boxes.cls):
-#                 left, top, right, bottom = bbox.tolist()
-#             event_bodies.append({
-#                 'Label': cls.item(),
-#                 'Left': left,
-#                 'Top': top,
-#                 'Right': right,
-#                 'Bottom': bottom
-#             })
-
-#         _Event = {
-#             'EventHeader': {
-#                 'UserId': userId,
-#                 'CameraId': cameraId,
-#                 'Created': created,
-#                 'Path': path,
-#                 'IsRequiredObjectDetection': False
-#             },
-#             'EventBodies': event_bodies,
-#         }
-
-#         image.close()
-#         event_dict = _Event.dict()
-#         return  JSONResponse(content=event_dict)
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/api/event/events")
 async def create_event(request: Request):
@@ -91,15 +45,12 @@ async def create_event(request: Request):
             },
             EventBodies=event_bodies,
         )
-        
+
         image.close()
         event_dict = _Event.dict()
         return  JSONResponse(content=event_dict)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-
+        return JSONResponse(status_code=500, content={"message": str(e)})
 
 
 if __name__ == "__main__":
