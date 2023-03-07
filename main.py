4163ceff9743313179db14388181885b78aaca68
Yurve <https://github.com/Yurve>
2023-03-07 16:57:01 +0900

    Test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.exceptions import HTTPException
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
@@ -8,51 +8,55 @@ from PIL import Image
 
 app = FastAPI() 
 
-@app.post("/api/event/events")
-async def create_event(event: Event):
-    if event.EventHeader is None:
-        raise HTTPException(status_code=422, detail="EventHeader is required")
+# @app.post("/api/event/events")
+# async def create_event(event: Event):
+#     if event.EventHeader is None:
+#         raise HTTPException(status_code=422, detail="EventHeader is required")
     
-    model = YOLO("yolov8l.pt")
+#     model = YOLO("yolov8l.pt")
 
-    try:
-        userId = event.EventHeader.UserId
-        cameraId = event.EventHeader.CameraId
-        created = event.EventHeader.Created
-        path = event.EventHeader.Path
-        image = Image.open(path)
+#     try:
+#         userId = event.EventHeader.UserId
+#         cameraId = event.EventHeader.CameraId
+#         created = event.EventHeader.Created
+#         path = event.EventHeader.Path
+#         image = Image.open(path)
 
-        results = model.predict(image)
+#         results = model.predict(image)
 
-        event_bodies = []
-        for result in results:
-            for bbox, cls in zip(result.boxes.xyxy, result.boxes.cls):
-                left, top, right, bottom = bbox.tolist()
-            event_bodies.append({
-                'Label': cls.item(),
-                'Left': left,
-                'Top': top,
-                'Right': right,
-                'Bottom': bottom
-            })
+#         event_bodies = []
+#         for result in results:
+#             for bbox, cls in zip(result.boxes.xyxy, result.boxes.cls):
+#                 left, top, right, bottom = bbox.tolist()
+#             event_bodies.append({
+#                 'Label': cls.item(),
+#                 'Left': left,
+#                 'Top': top,
+#                 'Right': right,
+#                 'Bottom': bottom
+#             })
 
-        _Event = {
-            'EventHeader': {
-                'UserId': userId,
-                'CameraId': cameraId,
-                'Created': created,
-                'Path': path,
-                'IsRequiredObjectDetection': False
-            },
-            'EventBodies': event_bodies,
-        }
+#         _Event = {
+#             'EventHeader': {
+#                 'UserId': userId,
+#                 'CameraId': cameraId,
+#                 'Created': created,
+#                 'Path': path,
+#                 'IsRequiredObjectDetection': False
+#             },
+#             'EventBodies': event_bodies,
+#         }
 
-        image.close()
-        event_dict = _Event.dict()
-        return  JSONResponse(content=event_dict)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+#         image.close()
+#         event_dict = _Event.dict()
+#         return  JSONResponse(content=event_dict)
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
 
+@app.post("/api/event/events")
+async def create_event(request: Request):
+    event = await request.json()
+    print(event)
 
 
 
