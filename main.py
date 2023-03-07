06699bab64575ac7e133f290d2256ee9e96216bf
Yurve <https://github.com/Yurve>
2023-03-07 17:00:20 +0900

    test

--- main.py
+++ main.py
@@ -56,7 +56,47 @@ app = FastAPI()
 @app.post("/api/event/events")
 async def create_event(request: Request):
     event = await request.json()
-    print(event)
+    if event['EventHeader'] is None:
+        raise HTTPException(status_code=422, detail="EventHeader is required")
+    
+    model = YOLO("yolov8l.pt")
+    try:
+        userId = event['EventHeader']['UserId']
+        cameraId = event['EventHeader']['CameraId']
+        created = event['EventHeader']['Created']
+        path = event['EventHeader']['Path']
+        image = Image.open(path)
+
+        results = model.predict(image)
+
+        event_bodies = []
+        for result in results:
+            for bbox, cls in zip(result.boxes.xyxy, result.boxes.cls):
+                left, top, right, bottom = bbox.tolist()
+            event_bodies.append({
+                'Label': cls.item(),
+                'Left': left,
+                'Top': top,
+                'Right': right,
+                'Bottom': bottom
+            })
+
+        _Event = {
+            'EventHeader': {
+                'UserId': userId,
+                'CameraId': cameraId,
+                'Created': created,
+                'Path': path,
+                'IsRequiredObjectDetection': False
+            },
+            'EventBodies': event_bodies,
+        }
+
+        image.close()
+        event_dict = _Event.dict()
+        return  JSONResponse(content=event_dict)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 
