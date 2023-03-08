b99e10d01e06eb143e398ca4eb55344c74a5d697
Yurve <https://github.com/Yurve>
2023-03-08 14:14:13 +0900

    Test

--- main.py
+++ main.py
@@ -10,19 +10,12 @@ app = FastAPI(debug=False)
 model = YOLO("fireLargeV8.pt")
 
 @app.post("/api/event/events/")
-async def create_event(request: Request):
-    try:
-        event_header = request.data['EventHeader']
-    except KeyError:
-        return JSONResponse(status_code=400, content={'Error': 'EventHeader key not found.'})
-
-    try:
-        userId = event_header['UserId']
-        cameraId = event_header['CameraId']
-        created = event_header['Created']
-        path = event_header['Path']
-    except KeyError:
-        return JSONResponse(status_code=400, content={'Error': 'UserId, CameraId, Created, or Path key not found in EventHeader.'})
+async def create_event(event: Event):
+    
+    userId = event.EventHeader.UserId
+    cameraId = event.EventHeader.CameraId
+    created = event.EventHeader.Created
+    path = event.EventHeader.Path
 
     try:
         image = Image.open(path)
